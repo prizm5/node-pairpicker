49d4e48a5d8702c7c8f69540d51af8c90e25b217
Nils Creque <ncreque@us.ibm.com>
2015-11-17 08:54:21 -0500

    more changes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var p = require('./pairpicker.js');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,12 +11,36 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.post('/', function(req, res) {
+    console.log(req.body.test);
+    var g = p.picker.generatePairs(["Nilhouse","Max"]);
+    console.log(g);
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
---
+++ pairpicker.js
@@ -0,0 +1,39 @@
+var picker =  {};
+
+picker.generatePairs = function(names) {
+
+	var shuffledPairs = shuffle(names);
+
+	var splitArray = splitarray(shuffledPairs, 2);
+
+	for(i=0; i< splitArray.length; i++){
+			console.log(splitArray[i]);
+	}
+};
+
+picker.shuffle = function(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+};
+
+picker.splitarray = function(input, spacing)
+{
+    var output = [];
+    for (var i = 0; i < input.length; i += spacing) {
+        output[output.length] = input.slice(i, i + spacing);
+    }
+    return output;
+};
