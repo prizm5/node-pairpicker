02fd35d606da6581bf116ec12056a0faebfcee95
Nils Creque <ncreque@us.ibm.com>
2016-02-05 07:33:33 -0500

    adding modal save diaglog

--- app/components/pairs.component.ts
+++ app/components/pairs.component.ts
@@ -22,7 +22,6 @@ import {Pairing} from '../models/pair'
                             </tr>
                         </tbody>
                      </table>
-                    
                 </div>
                 <div class="col-sm-4 text-center">
                     <h3>Odd</h3>
@@ -34,11 +33,33 @@ import {Pairing} from '../models/pair'
             </div>
             <div class="row">
                 <hr>
-                <div class="col-sm-3 portfolio-item">
-                    <a href="#pairs">
+                <div class="col-sm-1 portfolio-item">
+                    
+                    <a href="#myModal" class="portfolio-link" data-toggle="modal">
                         <button type="submit" class="btn btn-primary btn-lg" (click)="savePair()">Save</button>
                     </a>
                 </div>
+                 <!-- Modal -->
+                            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                            <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary">Save changes</button>
+                                        </div>
+                                    </div>
+                                    <!-- /.modal-content -->
+                                </div>
+                                <!-- /.modal-dialog -->
+                            </div>
+                            <!-- /.modal -->
             </div>
         </div>
     </section>
