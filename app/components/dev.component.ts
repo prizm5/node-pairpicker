3e3018522c5fd9224424ae54f1280d29e36e3ef9
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-03-11 10:35:57 -0500

    Fix template indentation

--- app/components/dev.component.ts
+++ app/components/dev.component.ts
@@ -8,15 +8,15 @@ import {Team} from '../models/team'
     template: `
     <div class="input-group dev" *ngFor="#peep of peeps">
         <span class="input-group-addon" *ngIf="peep.shouldPair">
-           <div class="cbx cbx-md cbx-active" tabindex="1000">
-           <span class="cbx-icon">
-            <div [ngSwitch]="peep.state">
-                    <template ngSwitchDefault><i class="glyphicon glyphicon-ok" (click)="onToggleCheckbox(peep,1)"></i></template>
-                    <template [ngSwitchWhen]="1"><i class="glyphicon glyphicon-user" (click)="onToggleCheckbox(peep,2)"></i></template>
-                    <template [ngSwitchWhen]="2"><i class="glyphicon glyphicon-stop" (click)="onToggleCheckbox(peep,3)"></i></template>
-                    <template [ngSwitchWhen]="3"><i class="glyphicon glyphicon-remove" (click)="onToggleCheckbox(peep,0)"></i></template>
-                </div>
-            </span>
+            <div class="cbx cbx-md cbx-active" tabindex="1000">
+                <span class="cbx-icon">
+                    <div [ngSwitch]="peep.state">
+                        <template ngSwitchDefault><i class="glyphicon glyphicon-ok" (click)="onToggleCheckbox(peep,1)"></i></template>
+                        <template [ngSwitchWhen]="1"><i class="glyphicon glyphicon-user" (click)="onToggleCheckbox(peep,2)"></i></template>
+                        <template [ngSwitchWhen]="2"><i class="glyphicon glyphicon-stop" (click)="onToggleCheckbox(peep,3)"></i></template>
+                        <template [ngSwitchWhen]="3"><i class="glyphicon glyphicon-remove" (click)="onToggleCheckbox(peep,0)"></i></template>
+                    </div>
+                </span>
             </div>
         </span>
         <div type="text" class="form-control" aria-label="...">{{peep.name}}</div>
