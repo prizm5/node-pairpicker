eeda4ae8d01e6bbbfe2f2acd065a068af40b4ec3
Patrick Boe & Max P Haley <mphaley@us.ibm.com>
2016-03-23 14:04:33 -0400

    startng to add routing

--- app/boot.ts
+++ app/boot.ts
@@ -2,6 +2,7 @@ import {bootstrap}        from 'angular2/platform/browser';
 import {HTTP_PROVIDERS}   from 'angular2/http';
 import {AppComponent}     from './app.component';
 import {NameService}      from './services/names.service';
+import {ROUTER_PROVIDERS} from 'angular2/router';
 
-bootstrap(AppComponent, [HTTP_PROVIDERS, NameService])
+bootstrap(AppComponent, [HTTP_PROVIDERS, NameService, ROUTER_PROVIDERS])
   .catch(err => console.error(err));
