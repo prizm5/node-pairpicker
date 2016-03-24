db71fe855f21e61de11340d89f89a5db09f7a591
Patrick Boe & Max P Haley <mphaley@us.ibm.com>
2016-03-24 11:32:20 -0400

    added pairing stats page

--- app/stats.component.js
+++ app/stats.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './services/names.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,23 +10,44 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, names_service_1;
     var Stats;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (names_service_1_1) {
+                names_service_1 = names_service_1_1;
             }],
         execute: function() {
             Stats = (function () {
-                function Stats() {
+                function Stats(_nameService) {
+                    this._nameService = _nameService;
                 }
+                ;
+                Stats.prototype.getPairCounts = function (retry) {
+                    var _this = this;
+                    if (retry === void 0) { retry = 0; }
+                    this._nameService.getPairCounts().subscribe(function (n) {
+                        _this.paircounts = n;
+                    }, function (error) {
+                        retry++;
+                        if (retry < 4)
+                            _this.getPairCounts(retry);
+                        console.error(error);
+                    });
+                };
+                Stats.prototype.ngOnInit = function () {
+                    this.getPairCounts();
+                };
                 Stats = __decorate([
                     core_1.Component({
                         styles: [],
-                        template: "\n  <!-- Stats Grid Section -->\n    <section id=\"stats\">\n      <div class=\"container\">\n        <div class=\"row\" >\n          <div class=\"col-lg-12 text-center\">\n            <h2>Statistics</h2>\n            <hr class=\"star-primary\">\n          </div>\n        </div>\n        <div class=\"row\">\n          Hellllllooooo Stats\n        </div>\n\n      </div>\n    </section>\n\n  "
+                        template: "\n  <!-- Stats Grid Section -->\n    <section id=\"stats\">\n      <div class=\"container\">\n        <div class=\"row\" >\n          <div class=\"col-lg-12 text-center\">\n            <h2>Statistics</h2>\n            <hr class=\"star-primary\">\n          </div>\n        </div>\n        <div class=\"row\">\n          <div class=\"col-lg-12 text-center\">\n            <table class=\"table table-striped table-bordered dataTable no-footer text-left\" id=\"dataTables-example\" role=\"grid\">\n              <thead>\n                  <tr role=\"row\">\n                    <th class=\"sorting\" tabindex=\"0\" aria-controls=\"dataTables-example\" rowspan=\"1\" colspan=\"1\" >Name</th>\n                    <th class=\"sorting\" tabindex=\"0\" aria-controls=\"dataTables-example\" rowspan=\"1\" colspan=\"1\" >Pairs</th>\n                    <th class=\"sorting\" tabindex=\"0\" aria-controls=\"dataTables-example\" rowspan=\"1\" colspan=\"1\" >Odd</th>\n                    <th class=\"sorting\" tabindex=\"0\" aria-controls=\"dataTables-example\" rowspan=\"1\" colspan=\"1\" >Last</th>\n                  </tr>\n              </thead>\n              <tbody>\n                <tr class=\"gradeA odd stat-row\" role=\"row\" *ngFor=\"#stat of paircounts\">\n                  <td>{{stat.key}}</td>\n                  <td>{{stat.value.pairing ? stat.value.pairing.count : 0}}</td>\n                  <td>{{stat.value.odd ? stat.value.odd.count   : 0}}</td>\n                  <td>{{ 'N/A' }}</td>\n                </tr></tbody>\n            </table>\n          </div>\n        </div>\n      </div>\n    </section>\n  ",
+                        inputs: ['paircounts'],
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [names_service_1.NameService])
                 ], Stats);
                 return Stats;
             }());
--- app/stats.component.ts
+++ app/stats.component.ts
@@ -1,4 +1,5 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit}    from 'angular2/core';
+import {NameService}  from './services/names.service'
 
 @Component({
   styles: [],
@@ -13,13 +14,47 @@ import {Component} from 'angular2/core';
           </div>
         </div>
         <div class="row">
-          Hellllllooooo Stats
+          <div class="col-lg-12 text-center">
+            <table class="table table-striped table-bordered dataTable no-footer text-left" id="dataTables-example" role="grid">
+              <thead>
+                  <tr role="row">
+                    <th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" >Name</th>
+                    <th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" >Pairs</th>
+                    <th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" >Odd</th>
+                    <th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" >Last</th>
+                  </tr>
+              </thead>
+              <tbody>
+                <tr class="gradeA odd stat-row" role="row" *ngFor="#stat of paircounts">
+                  <td>{{stat.key}}</td>
+                  <td>{{stat.value.pairing ? stat.value.pairing.count : 0}}</td>
+                  <td>{{stat.value.odd ? stat.value.odd.count   : 0}}</td>
+                  <td>{{ 'N/A' }}</td>
+                </tr></tbody>
+            </table>
+          </div>
         </div>
-
       </div>
     </section>
-
-  `
+  `,
+  inputs: ['paircounts'],
 })
 
-export class Stats { }
+export class Stats {
+  public paircounts: any;;
+  constructor(private _nameService: NameService) { }
+  getPairCounts(retry: number = 0): void {
+    this._nameService.getPairCounts().subscribe(
+      n => {
+        this.paircounts = n;
+      },
+      error => {
+        retry++;
+        if (retry < 4) this.getPairCounts(retry);
+        console.error(error);
+      });
+  }
+   ngOnInit(): void {
+    this.getPairCounts();
+  }
+ }
